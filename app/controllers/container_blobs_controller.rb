b3be32694986b193c10af1ebc57e205f6069eab8
william <william.mccumstie@outlook.com>
2019-02-26 14:47:44 +0000

    Add a route for showing a single Blob
    This allows for a particular package to be referenced

--- app/controllers/container_blobs_controller.rb
+++ app/controllers/container_blobs_controller.rb
@@ -6,9 +6,17 @@ class ContainerBlobsController < ApplicationController
     render jsonapi: container.packages_blobs
   end
 
+  def show
+    render jsonapi: blob_param
+  end
+
   private
 
   def container
     Container.find(params.require(:container_id))
+  end
+
+  def blob_param
+    container.packages_blobs.find(params.require(:id))
   end
 end
