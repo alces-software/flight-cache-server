4892f51d512a93d32b556d0c069418617eabde86
william <william.mccumstie@outlook.com>
2019-03-02 17:25:08 +0000

    Update the cancan macros into steps
    This gets the controller ready for the routes update

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -2,8 +2,12 @@ require 'active_storage/blob'
 
 class BlobsController < ApplicationController
   load_and_authorize_resource :blob, only: [:show, :download]
-  load_and_authorize_resource :container, only: :index
-  load_and_authorize_resource :blob, through: :container, only: :index
+
+  # On Index, load the blobs via a container
+  only_index = { only: :index }
+  load_resource :container, **only_index
+  authorize_resource :container, **only_index
+  load_and_authorize_resource :blob, through: :container, **only_index
 
   def index
     serial = BlobSerializer.new(@blobs, is_collection: true)
