24b4e558f259e8ef75aa4e4809bb6b6f7845bdb2
william <william.mccumstie@outlook.com>
2019-03-02 19:42:26 +0000

    Prepare Users to become a many to many

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   belongs_to :group, optional: true
 
   has_many :containers, through: :group
+
+  # Adds the "groups" method so it can be used in the refactoring. This will
+  # likely become an many-to-many relationship
+  def groups
+    [group].reject(&:nil?)
+  end
 end
