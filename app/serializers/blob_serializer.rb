928aa3330063c3ae2285c0ce76433553dc115798
william <william.mccumstie@outlook.com>
2019-03-05 17:24:11 +0000

    Regularize the syntax for blocks

--- app/serializers/blob_serializer.rb
+++ app/serializers/blob_serializer.rb
@@ -1,12 +1,12 @@
 class BlobSerializer < ApplicationSerializer
   attributes :checksum
-  attribute :size, &:byte_size
-  attribute :filename { |o| o.filename.to_s }
+  attribute(:size, &:byte_size)
+  attribute(:filename) { |o| o.filename.to_s }
 
   belongs_to :container, links: {
     related: proc { |b| urls.url_for(b.container) }
   }
 
-  link :self { |b| urls.blob_url(b) }
-  link :download { |b| urls.download_blob_url(b) }
+  link(:self) { |b| urls.blob_url(b) }
+  link(:download) { |b| urls.download_blob_url(b) }
 end
