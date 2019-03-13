53754369b3d3e0f9e1d486786c9954ebf8897e8f
william <william.mccumstie@outlook.com>
2019-03-13 14:42:44 +0000

    Do not rename the size field during serialization
    This causes issues on the client as `size` is a reserved word. Instead
    `byte_size` should be sent as the raw key. The aliasing can be done by
    the client

--- app/serializers/blob_serializer.rb
+++ app/serializers/blob_serializer.rb
@@ -1,6 +1,6 @@
 class BlobSerializer < ApplicationSerializer
   attributes :checksum
-  attribute(:size, &:byte_size)
+  attribute(:byte_size)
   attribute(:filename) { |o| o.filename.to_s }
 
   belongs_to :container, links: {
