893679a0e91ab9fd7bff4d3447a8bd960b3376da
william <william.mccumstie@outlook.com>
2019-03-18 17:24:09 +0000

    Split unique index into its Client and Group parts
    It is not possible to index on a NULL value in the db. This causes the
    entire index as `user` or `group` will be NULL.
    
    Instead the user and group will be index separately

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_122733) do
+ActiveRecord::Schema.define(version: 2019_03_18_172029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,8 @@ ActiveRecord::Schema.define(version: 2019_03_18_122733) do
     t.bigint "access_tag_id"
     t.bigint "group_id"
     t.bigint "user_id"
-    t.index ["access_tag_id", "group_id", "user_id"], name: "index_containers_on_access_tag_id_and_group_id_and_user_id", unique: true
+    t.index ["access_tag_id", "group_id"], name: "index_containers_on_access_tag_id_and_group_id", unique: true
+    t.index ["access_tag_id", "user_id"], name: "index_containers_on_access_tag_id_and_user_id", unique: true
     t.index ["access_tag_id"], name: "index_containers_on_access_tag_id"
     t.index ["group_id"], name: "index_containers_on_group_id"
     t.index ["user_id"], name: "index_containers_on_user_id"
