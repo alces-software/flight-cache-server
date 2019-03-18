36ff998f332c151ab9c79b7b94a19afa3c330b2c
william <william.mccumstie@outlook.com>
2019-03-18 14:35:10 +0000

    Correctly enforce the new relationship on the Models
    Containers can now belong to either a User xor Group. Also a User can
    now have to classes if containers: group or user. These are merged
    together to get the complete list of containers

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,7 +1,15 @@
 require 'aws-sdk-s3'
 
 class Container < ApplicationRecord
+  validates :user, absence: true, if: :group
+  validates :user, presence: true, unless: :group
+
+  validates :group, absence: true, if: :user
+  validates :group, presence: true, unless: :user
+
   has_many :blobs
   belongs_to :access_tag
-  belongs_to :group
+
+  belongs_to :group, optional: true
+  belongs_to :user, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  has_many :user_containers, class_name: 'Container'
+
   belongs_to :group, optional: true
 
-  has_many :containers, through: :group
+  def group_containers
+    group&.containers || []
+  end
+
+  def containers
+    [user_containers, group_containers].flatten
+  end
 
   # Adds the "groups" method so it can be used in the refactoring. This will
   # likely become an many-to-many relationship
