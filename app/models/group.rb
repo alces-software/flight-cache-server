7306ee1d253f46f13df50ce3a7e7065826651aca
william <william.mccumstie@outlook.com>
2019-03-28 12:50:29 +0000

    Remove the overloading of the `users` method
    This causes confusion as it hides which user is attached to the group.
    Instead the `readble?` and `writable?` methods handle access to the
    public group.
    
    As global admin access is controlled by `cancan`, the models do not need
    to worry about this edge case. As such the public group is not
    `writable?` to anyone.

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,15 +4,23 @@ class Group < ApplicationRecord
   has_many :containers
   has_many :users, foreign_key: 'default_group_id'
 
-  def users
-    name == 'public' ? User.all : super
-  end
-
   def readable?(other)
-    users.include?(other)
+    if self.public?
+      true
+    else
+      users.include?(other)
+    end
   end
 
   def writable?(other)
-    readable?(other)
+    if self.public?
+      false
+    else
+      user.include?(other)
+    end
+  end
+
+  def public?
+    name == 'public'
   end
 end
