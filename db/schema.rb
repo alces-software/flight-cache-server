c7d26c93402d782a203937036a444f48a3c53951
william <william.mccumstie@outlook.com>
2019-04-08 15:27:44 +0100

    Change the blob table to have a regular primary key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_190218) do
+ActiveRecord::Schema.define(version: 2019_04_08_140911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,8 @@ ActiveRecord::Schema.define(version: 2019_04_02_190218) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "blobs", primary_key: "active_storage_blob_id", id: :serial, force: :cascade do |t|
+  create_table "blobs", force: :cascade do |t|
+    t.serial "active_storage_blob_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "container_id", null: false
